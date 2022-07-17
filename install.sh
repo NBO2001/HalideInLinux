c983c49d9e8b9893ba583beb7170ad53dae303be
NatanaelB <natanaelbezerra36@gmail.com>
2022-07-17 13:32:14 -0400

    sem -y

--- install.sh
+++ install.sh
@@ -12,7 +12,7 @@ sudo apt install \
                   clang-tools lld llvm-dev libclang-dev \
                   libpng-dev libjpeg-dev libgl-dev \
                   python3-dev python3-numpy python3-scipy python3-imageio \
-                  libopenblas-dev libeigen3-dev libatlas-base-dev -y
+                  libopenblas-dev libeigen3-dev libatlas-base-dev
 
 sudo wget https://github.com/halide/Halide/releases/download/v14.0.0/Halide-14.0.0-x86-64-linux-6b9ed2afd1d6d0badf04986602c943e287d44e46.tar.gz
 
